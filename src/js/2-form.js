0c4119095ac2bc5e98df53374d5c27a6f919a656
OleksandrYaromenko <oleksandryaromenko21@gmail.com>
2024-03-31 17:54:32 +0200

    готова дз

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,29 @@
-console.log("eeeeee")
+const INPUT_KEY = "input"
+const TEXTAREA_KEY = "textarea"
+const feedbackForm = document.querySelector(".feedback-form");
+const input = feedbackForm.querySelector("input");
+const textarea = feedbackForm.querySelector("textarea");
+const saveToLocalStorage = (key, event) => {
+    const message = event.target.value;
+    localStorage.setItem(key, message);
+};
+getLocalStorage ()
+input.addEventListener("input", saveToLocalStorage.bind(null, "input"));
+textarea.addEventListener("input", saveToLocalStorage.bind(null, "textarea"))
+feedbackForm.addEventListener("submit",resetSubmit)
+function getLocalStorage () {
+    const nameGet = localStorage.getItem(INPUT_KEY)
+    const messageGet = localStorage.getItem(TEXTAREA_KEY)
+    if (nameGet,messageGet) {
+        input.value = nameGet
+        textarea.value = messageGet
+    }
+    console.log(nameGet)
+}
+function resetSubmit(event) {
+    event.preventDefault()
+    event.currentTarget.reset()
+    localStorage.removeItem(INPUT_KEY)
+    localStorage.removeItem(TEXTAREA_KEY)
+}
+
